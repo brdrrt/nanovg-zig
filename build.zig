a262d4b7cf75dd98d891da8f61037d4d11b8bad5
Robert Bridda <briddarobert+github@icloud.com>
2024-07-13 11:19:11 +0200

    Add zig package manager support

--- build.zig
+++ build.zig
@@ -4,34 +4,34 @@ pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const nanovg_mod = b.addModule("nanovg", .{
+    const nanovg = b.addModule("nanovg", .{
         .root_source_file = .{ .src_path = .{ .owner = b, .sub_path = "src/nanovg.zig" } },
         .target = target,
         .optimize = optimize,
         .link_libc = true,
     });
-    nanovg_mod.addIncludePath(.{ .src_path = .{ .owner = b, .sub_path = "src" } });
-    nanovg_mod.addIncludePath(.{ .src_path = .{ .owner = b, .sub_path = "lib/gl2/include" } });
-    nanovg_mod.addCSourceFile(.{ .file = .{ .src_path = .{ .owner = b, .sub_path = "src/fontstash.c" } }, .flags = &.{ "-DFONS_NO_STDIO", "-fno-stack-protector" } });
-    nanovg_mod.addCSourceFile(.{ .file = .{ .src_path = .{ .owner = b, .sub_path = "src/stb_image.c" } }, .flags = &.{ "-DSTBI_NO_STDIO", "-fno-stack-protector" } });
+    nanovg.addIncludePath(.{ .src_path = .{ .owner = b, .sub_path = "src" } });
+    nanovg.addIncludePath(.{ .src_path = .{ .owner = b, .sub_path = "lib/gl2/include" } });
+    nanovg.addCSourceFile(.{ .file = .{ .src_path = .{ .owner = b, .sub_path = "src/fontstash.c" } }, .flags = &.{ "-DFONS_NO_STDIO", "-fno-stack-protector" } });
+    nanovg.addCSourceFile(.{ .file = .{ .src_path = .{ .owner = b, .sub_path = "src/stb_image.c" } }, .flags = &.{ "-DSTBI_NO_STDIO", "-fno-stack-protector" } });
 
     if (target.result.isWasm()) {
-        _ = installDemo(b, target, optimize, "demo", "examples/example_wasm.zig", nanovg_mod);
+        _ = installDemo(b, target, optimize, "demo", "examples/example_wasm.zig", nanovg);
     } else {
-        _ = installDemo(b, target, optimize, "demo_glfw", "examples/example_glfw.zig", nanovg_mod);
-        _ = installDemo(b, target, optimize, "demo_fbo", "examples/example_fbo.zig", nanovg_mod);
-        _ = installDemo(b, target, optimize, "demo_clip", "examples/example_clip.zig", nanovg_mod);
+        _ = installDemo(b, target, optimize, "demo_glfw", "examples/example_glfw.zig", nanovg);
+        _ = installDemo(b, target, optimize, "demo_fbo", "examples/example_fbo.zig", nanovg);
+        _ = installDemo(b, target, optimize, "demo_clip", "examples/example_clip.zig", nanovg);
     }
 }
 
-fn installDemo(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, name: []const u8, root_source_file: []const u8, nanovg_mod: *std.Build.Module) *std.Build.Step.Compile {
+fn installDemo(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.builtin.OptimizeMode, name: []const u8, root_source_file: []const u8, nanovg: *std.Build.Module) *std.Build.Step.Compile {
     const demo = b.addExecutable(.{
         .name = name,
         .root_source_file = .{ .src_path = .{ .owner = b, .sub_path = root_source_file } },
         .target = target,
         .optimize = optimize,
     });
-    demo.root_module.addImport("nanovg", nanovg_mod);
+    demo.root_module.addImport("nanovg", nanovg);
 
     demo.addIncludePath(.{ .src_path = .{ .owner = b, .sub_path = "examples" } });
     demo.addCSourceFile(.{ .file = .{ .src_path = .{ .owner = b, .sub_path = "examples/stb_image_write.c" } }, .flags = &.{ "-DSTBI_NO_STDIO", "-fno-stack-protector" } });
